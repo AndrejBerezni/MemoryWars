91c01cd87649e452869a19921465c2e93dec66ff
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-06-12 14:19:37 +0100

    configure routes on navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './App.css';
-
-import Navigation from './components/Navigation';
-import Game from './components/Game';
-import Highscores from './components/Highscores';
-import Score from './components/Score';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Game from "./components/Game";
+import Highscores from "./components/Highscores";
+import Score from "./components/Score";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-     <Navigation />
-     <Game />
-     <Score />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Game />}/>
+        <Route path="/highscores" element={<Highscores />}/>
+        <Route />
+      </Routes>
+      <Score />
     </>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Navigation() {
     return (
         <>
-        <a>GAME</a>
-        <a>HIGHSCORES</a>
+            <Link to="/" >GAME</Link>
+            <Link to="/highscores">HIGHSCORES</Link>
         </>
     )
 }
