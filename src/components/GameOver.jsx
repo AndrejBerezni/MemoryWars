603db969df55da63b0c95c7564dc9180899240b9
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-06-14 16:34:55 +0100

    write scores to database on form submit

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,12 +4,25 @@ import { useSelector, useDispatch } from 'react-redux';
 import { resetScore } from '../redux_actions/scoreActions';
 import { startGame } from '../redux_actions/gameActions';
 import { resetTimer } from '../redux_actions/timerActions';
+import { addScore } from '../config/firebase';
 
 export default function GameOver() {
     const dispatch = useDispatch()
     const score = useSelector(state => state.score);
     const time = useSelector(state => state.timer.value);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const playerName = e.target.elements['player-name'].value;
+        await addScore({
+          name: playerName,
+          score: score,
+          time: time
+        });
+        dispatch(resetScore());
+        dispatch(resetTimer());
+        dispatch(startGame());
+    }
     return (
         <div className='game-over'>
             <h1>GAME OVER</h1>
@@ -19,15 +32,10 @@ export default function GameOver() {
                 :
                 {time - Math.floor(time / 60) * 60 > 9 ? Math.round(time - Math.floor(time / 60) * 60) : `0${Math.round(time - Math.floor(time / 60) * 60)}`}
             </h2>
-            <form>
-                <label for='player-name'>Your name:</label>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='player-name'>Your name:</label>
                 <input id='player-name' name='player-name' type='text' max='30' required/>
-                <button type='submit' onClick={(e) => {
-                    e.preventDefault();
-                    dispatch(resetScore());
-                    dispatch(resetTimer());
-                    dispatch(startGame())
-                }}>Submit</button>
+                <button type='submit'>Submit</button>
             </form>
         </div>
     )
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore, collection, doc, addDoc, query, orderBy, limit, getDocs} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB8cYPrqvF-GmAL9SjjEPspohL0uQbqJbw",
@@ -6,7 +7,35 @@ const firebaseConfig = {
   projectId: "memorywarsgame",
   storageBucket: "memorywarsgame.appspot.com",
   messagingSenderId: "491419156025",
-  appId: "1:491419156025:web:3cb91f7cfcdc06a08c17e8"
+  appId: "1:491419156025:web:3cb91f7cfcdc06a08c17e8",
 };
 
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+async function getScores() {
+  let scoresArray = [];
+  const q = query(
+    collection(db, "Highscores"),
+    orderBy('score', 'desc'),
+    orderBy('time', 'asc'),
+    limit(10)
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach(doc => {
+    scoresArray.push(doc.data());
+  });
+  return scoresArray;
+};
+
+async function addScore(entry) {
+  const highscoresCollectionRef = collection(db, "Highscores")
+  await addDoc(highscoresCollectionRef, {
+    name: entry.name,
+    score: entry.score,
+    time: entry.time
+  })
+}
+
+export {getScores, addScore}
