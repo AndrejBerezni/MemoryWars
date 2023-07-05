fb788e274efae773d4642b384c3f3626357e40c8
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-07-05 15:20:08 +0100

    add animation to GameOver component

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -16,9 +16,9 @@ export default function GameOver() {
         e.preventDefault();
         const playerName = e.target.elements['player-name'].value;
         await addScore({
-          name: playerName,
-          score: score,
-          time: time
+            name: playerName,
+            score: score,
+            time: time
         });
         dispatch(resetScore());
         dispatch(resetTimer());
@@ -26,31 +26,90 @@ export default function GameOver() {
     }
     return (
         <motion.div
-        className='game-over'
-        animate = {{
-            scale: 1,
-            opacity: 1
-        }}
-        initial={{
-            scale: 0,
-            opacity: 0
-        }}
-        transition={{
-            duration: 0.5
-        }}
+            className='game-over'
+            animate={{
+                scale: 1,
+                opacity: 1
+            }}
+            initial={{
+                scale: 0,
+                opacity: 0
+            }}
+            transition={{
+                duration: 0.2
+            }}
         >
-            <h1>GAME OVER</h1>
-            <h2>Total guesses: {score}</h2>
-            <h2>
+            <motion.h1
+                className='game-over-title'
+                animate={{
+                    scale: 1,
+                    opacity: 1
+                }}
+                initial={{
+                    scale: 0,
+                    opacity: 0
+                }}
+                transition={{
+                    duration: 1.2,
+                    type: 'spring',
+                }}
+            >
+                GAME OVER
+            </motion.h1>
+            <motion.h2
+                animate={{
+                    scale: 1,
+                    opacity: 1
+                }}
+                initial={{
+                    scale: 0,
+                    opacity: 0
+                }}
+                transition={{
+                    duration: 1.2,
+                    type: 'spring',
+                    delay: 0.8
+                }}
+            >Total guesses: {score}</motion.h2>
+            <motion.h2
+                animate={{
+                    scale: 1,
+                    opacity: 1
+                }}
+                initial={{
+                    scale: 0,
+                    opacity: 0
+                }}
+                transition={{
+                    duration: 1.2,
+                    type: 'spring',
+                    delay: 0.8
+                }}
+            >
                 Time: {Math.floor(time / 60) > 9 ? Math.floor(time / 60) : `0${Math.floor(time / 60)}`}
                 :
                 {time - Math.floor(time / 60) * 60 > 9 ? Math.round(time - Math.floor(time / 60) * 60) : `0${Math.round(time - Math.floor(time / 60) * 60)}`}
-            </h2>
-            <form onSubmit={handleSubmit}>
+            </motion.h2>
+            <motion.form
+                animate={{
+                    scale: 1,
+                    opacity: 1
+                }}
+                initial={{
+                    scale: 0,
+                    opacity: 0
+                }}
+                transition={{
+                    duration: 1.2,
+                    type: 'spring',
+                    delay: 1.4
+                }}
+                onSubmit={handleSubmit}
+            >
                 <label htmlFor='player-name'>Your name:</label>
-                <input id='player-name' name='player-name' type='text' max='30' required/>
+                <input id='player-name' name='player-name' type='text' max='30' required />
                 <button type='submit'>Submit</button>
-            </form>
+            </motion.form>
         </motion.div>
     )
 }
